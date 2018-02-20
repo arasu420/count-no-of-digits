fc9e9d56d33b563cd735f35f159b38e380ccc60e
arasu420 <36299277+arasu420@users.noreply.github.com>
2018-02-20 17:10:45 +0530

    Add files via upload

---
+++ digit.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n,a=0;
+cout<<"Enter any num : ";
+cin>>n;
+while(n>0)
+{
+n=n/10;
+a++;
+}
+cout<<"No. of digits in given number is: "<<a;
+return 0;
+}
